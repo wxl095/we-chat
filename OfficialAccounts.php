cc4e6719b1071de905376f06abbd14543b139fa7
wxl <297750182@qq.com>
2019-12-26 14:38:17 +0800

    wxl

---
+++ OfficialAccounts.php
@@ -0,0 +1,125 @@
+<?php
+
+include './SendRequest.php';
+
+class OfficialAccounts
+{
+    const ErrorLogFile = __DIR__ . '/OfficialAccountsError.log';
+    protected $appId;
+    protected $secret;
+    protected $token;
+
+    /**
+     * OfficialAccounts constructor.
+     * @param string $appId
+     * @param string $secret
+     * @param string $token
+     */
+    public function __construct(string $appId, string $secret, string $token='wxl')
+    {
+        $this->appId = $appId;
+        $this->secret = $secret;
+        $this->token = $token;
+    }
+
+    /**
+     * signature验证通过原样返回
+     * @return string
+     */
+    public function verification(): string
+    {
+        if ($this->checkSignature()) {
+            return $_GET['echostr'];
+        }
+    }
+
+    /**
+     * 检验signature
+     * @return bool
+     */
+    private function checkSignature(): bool
+    {
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
+
+        $token = $this->token;
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode($tmpArr);
+        $tmpStr = sha1($tmpStr);
+
+        if ($tmpStr == $signature) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * 获取access_token
+     * @return string
+     * @throws ErrorException
+     */
+    public function getAccessToken(): string
+    {
+        $redis = new Redis();
+        $redis->connect('127.0.0.1');
+        $accessToken = $redis->get($this->appId . '_AccessToken');
+        if (!$accessToken) {
+            $request = new SendRequest('https://api.weixin.qq.com/cgi-bin/token', ['grant_type' => 'client_credential', 'appid' => $this->appId, 'secret' => $this->secret]);
+            $result = $request->post();
+            $result = json_decode($result, true);
+            if (isset($result['errcode'])) {
+                switch ($result['errcode']) {
+                    case -1:
+                        $this->writeLog('获取access_token失败', '-1', '系统繁忙，此时请开发者稍候再试');
+                        break;
+                    case 40001:
+                        $this->writeLog('获取access_token失败', '40001', 'AppSecret错误或者AppSecret不属于这个公众号，请开发者确认AppSecret的正确性');
+                        break;
+                    case 40002:
+                        $this->writeLog('获取access_token失败', '40002', '请确保grant_type字段值为client_credential');
+                        break;
+                    case 40164:
+                        $this->writeLog('获取access_token失败', '40164', '调用接口的IP地址不在白名单中，请在接口IP白名单中进行设置。');
+                        break;
+                }
+                throw new ErrorException('获取accessToken失败');
+            }
+            $accessToken = $result['access_token'];
+            $redis->set($this->appId . '_AccessToken', $accessToken, 7200);
+        }
+        return $accessToken;
+    }
+
+    /**
+     * 创建自定义菜单
+     * @param array $menu
+     * @return bool
+     * @throws ErrorException
+     */
+    public function createMenu(array $menu)
+    {
+        $accessToken = $this->getAccessToken();
+        $request = new SendRequest("https://api.weixin.qq.com/cgi-bin/menu/create?access_token=$accessToken", json_encode($menu, JSON_UNESCAPED_UNICODE));
+
+        $result = json_decode($request->post(), true);
+        if ($result['errcode'] != 0 && $result['errmsg'] != 'ok') {
+            $this->writeLog('自定义菜单创建失败', $result['errcode'], $result['errmsg']);
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 微信服务器返回的错误写入日志
+     * @param string $title
+     * @param string $errorCode
+     * @param string $errorMessage
+     */
+    private function writeLog(string $title, string $errorCode, string $errorMessage)
+    {
+        file_put_contents(self::ErrorLogFile, date('Y-m-d H:i:s') . '  appid为：' . $this->appId . '的账号' . $title . '  errorCode:' . $errorCode . "\r\n" . 'errorMessage:' . $errorMessage . "\r\n\r\n");
+    }
+}
---
+++ SendRequest.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * Created by PhpStorm
+ * User: wxl
+ * Date: 19-7-19
+ * Time: 上午11:04
+ */
+
+class SendRequest
+{
+	private $request;
+	CONST TIMEOUT = 5;
+	protected $data;
+	protected $url;
+
+	/**
+	 * SendRequest constructor.
+	 * @param string $url
+	 * @param  $data
+	 * @throws Exception
+	 */
+	public function __construct(string $url, $data='')
+	{
+		$this->request = curl_init();
+		if (empty($url)) throw new Exception('url地址不能为空');
+		$this->data = $data;
+		$this->url = $url;
+	}
+
+	private function setRequest()
+	{
+		//在输出中不包含标头。
+		curl_setopt($this->request, CURLOPT_HEADER, false);
+		//false阻止CURL验证对等方的证书。可以使用该CURLOPT_CAINFO选项指定要验证的备用证书，也可以使用该选项指定证书目录 CURLOPT_CAPATH。
+		curl_setopt($this->request, CURLOPT_SSL_VERIFYPEER, false);
+		//1检查SSL对等证书中是否存在公用名。
+		//2检查是否存在公用名，并验证它是否与提供的主机名匹配。
+		//0不检查名称。在生产环境中，此选项的值应保持为2（默认值）。
+		curl_setopt($this->request, CURLOPT_SSL_VERIFYHOST, 0);
+		//TRUE将传输作为curl_exec（）的返回值的字符串返回，而不是直接输出。
+		curl_setopt($this->request, CURLOPT_RETURNTRANSFER, true);
+		//	允许cURL函数执行的最大秒数。
+		curl_setopt($this->request, CURLOPT_TIMEOUT, self::TIMEOUT);
+	}
+
+	public function post()
+	{
+		$this->setRequest();
+		//要获取的URL。使用curl_init（）初始化会话时也可以设置此项。
+		curl_setopt($this->request, CURLOPT_URL, $this->url);
+		//POST请求
+		curl_setopt($this->request, CURLOPT_POST, true);
+		//要在HTTP“POST”操作中发布的完整数据
+		if (is_array($this->data)) $this->data = http_build_query($this->data);
+		curl_setopt($this->request, CURLOPT_POSTFIELDS, $this->data);
+		return $this->exec();
+	}
+
+	public function get()
+	{
+		$this->setRequest();
+		//要获取的URL。使用curl_init（）初始化会话时也可以设置此项。
+		curl_setopt($this->request, CURLOPT_URL, $this->url . http_build_query($this->data));
+		return $this->exec();
+	}
+
+	protected function exec()
+	{
+		return curl_exec($this->request);
+	}
+
+	public function __destruct()
+	{
+		curl_close($this->request);
+	}
+}
